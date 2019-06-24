cd8a6cf4e3eb4fee9f666e32554635bcbb1bc540
Sayeeshwar <sayeeshwar.girish@gmail.com>
2019-06-24 12:05:46 +0530

    Add JS to launch bubble on click

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-window.addEventListener('load',()=>{
-    const sounds = document.querySelectorAll(".sound");
-    const pads = document.querySelectorAll(".pads div");
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#d63333",
+    "#11c229",
+    "#0099ff",
+    "#d5fa31",
+    "#5f5f5f",
+    "#db247a"
+  ];
 
-
-
-
-    pads.forEach((pad, index) => {
-
-        pad.addEventListener('click',()=>{
-            sounds[index].currentTime=0;
-            sounds[index].play();
-        })
+  //Create bubbles on click
+  const createBubbles = index => {
+    const bubble = document.createElement("div");
+    visual.append(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", () => {
+      visual.removeChild(bubble);
     });
-})
+  };
+
+  //Create sound on click
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", () => {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubbles(index);
+    });
+  });
+});
